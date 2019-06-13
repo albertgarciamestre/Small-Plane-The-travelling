b3021ac8f32115544a9e1b37a75edcf2e88a14a5
Albert Garcia Mestre <albert.garcia.2@enti.cat>
2019-06-13 20:07:58 +0200

    Moving small plane and searching of assets
    Small plane moves in a constant speed and can move right or left. Insertion of some assets in the scene.

---
+++ README.md
@@ -0,0 +1,17 @@
+# Endless-Flight
+A game developed in Unity, for .Net Framework and C# module.
+
+The project we are developing is a endless runner style game.
+We are devloping this project in the Unity game engine which uses the .Net Framework and we will program the scripts and game logic in C#.
+
+The vision for the game is a 3D endless runner game similiar to many games on the market such as Subway Surfers, Temple Run or Endless-Lake.
+The style will be quite different to those already avaiable as the game will revolve around the player controlling a plane, flying endlessly in order to achieve a high score. Achieving a new high score will be the main objective of the game. 
+The player will have control over the plane horizontally and  also if implementation is required, vertically. 
+The plane will fly over various landscapes/scences which will evolve during the game. These various scenes will be procedurly generated.
+
+In order to add complexity to the game there will be numerous objects which will spwan in the planes path that it will be required to avoid.
+
+Many features are under consideration at the moment, such as Facebook Integration, which would allow for highscore sharing. Also under development are a number of different gameplay features which could be implemented.
+
+
+The basic features outlined above are the main vision and focus of the project at the current moment.
---
+++ Small-Plane-The-Travelling/Assets/Scripts/Player.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody rb;
+    const float minimumSpeed = -1500;
+    const float maxHorizontalSpeed = 250;
+    float currentHorizontalSpeed = 0;
+    private float currentRotation = 0;
+    private Vector3 currentAngle;
+
+    //float maxHorizontalSpeed = 5000;
+    float bonusHorizontalSpeed = 0;
+    float boostHorizontalSpeed = 0;
+
+    private bool controlZone = true;
+ 
+
+    // Use this for initialization
+    void Start ()
+	{
+	    rb = GetComponent<Rigidbody>();
+	    currentAngle = rb.transform.eulerAngles;
+        //Animator anim = GetComponent<Animator>();
+        //anim.Play("TakeOff");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	    rb.AddRelativeForce(Vector3.forward  * Time.deltaTime * 50);
+
+	    if (Input.GetKey(KeyCode.A))
+	    {
+            //transform.Rotate(0, 0, Time.deltaTime * 50);
+            //currentRotation = Mathf.Lerp(45,currentRotation, Time.deltaTime * 50 );
+
+	        currentAngle = new Vector3(
+	            Mathf.LerpAngle(currentAngle.x, 0, Time.deltaTime),
+	            Mathf.LerpAngle(currentAngle.y, 0, Time.deltaTime),
+	            Mathf.LerpAngle(currentAngle.z, 45, Time.deltaTime));
+
+            currentHorizontalSpeed = Mathf.Lerp(currentHorizontalSpeed, -maxHorizontalSpeed + -bonusHorizontalSpeed + -boostHorizontalSpeed, Time.deltaTime );
+            
+	    }
+
+        if (Input.GetKey(KeyCode.D))
+	    {
+            // transform.Rotate(0,0,-Time.deltaTime * 50);
+	        currentAngle = new Vector3(
+	            Mathf.LerpAngle(currentAngle.x, 0, Time.deltaTime),
+	            Mathf.LerpAngle(currentAngle.y, 0, Time.deltaTime),
+	            Mathf.LerpAngle(currentAngle.z, -45, Time.deltaTime));
+
+            currentHorizontalSpeed = Mathf.Lerp(currentHorizontalSpeed, maxHorizontalSpeed + bonusHorizontalSpeed + boostHorizontalSpeed, Time.deltaTime );
+        }
+
+	    if (!Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
+	    {
+	        currentHorizontalSpeed = Mathf.Lerp(currentHorizontalSpeed, 0, Time.deltaTime / 0.1f);
+            // currentRotation = Mathf.Lerp(0, rb.rotation.z, Time.deltaTime * 50);
+	        currentAngle = new Vector3(
+	            Mathf.LerpAngle(currentAngle.x, 0, Time.deltaTime),
+	            Mathf.LerpAngle(currentAngle.y, 0, Time.deltaTime),
+	            Mathf.LerpAngle(currentAngle.z, 0, Time.deltaTime * 2));
+        }
+        
+	    if (transform.position.x < 150)
+	    {
+	        transform.position = new Vector3(150, transform.position.y, transform.position.z);
+	        currentHorizontalSpeed = 0;
+	        currentAngle = new Vector3(
+	            Mathf.LerpAngle(currentAngle.x, 0, Time.deltaTime),
+	            Mathf.LerpAngle(currentAngle.y, 0, Time.deltaTime),
+	            Mathf.LerpAngle(currentAngle.z, 0, Time.deltaTime * 2));
+            Input.ResetInputAxes();
+             
+        }
+
+	    if (transform.position.x > 350)
+	    {
+	        transform.position = new Vector3(350, transform.position.y, transform.position.z);
+	        currentHorizontalSpeed = 0;
+	        currentAngle = new Vector3(
+	            Mathf.LerpAngle(currentAngle.x, 0, Time.deltaTime),
+	            Mathf.LerpAngle(currentAngle.y, 0, Time.deltaTime),
+	            Mathf.LerpAngle(currentAngle.z, 0, Time.deltaTime * 2));
+            Input.ResetInputAxes();
+        }
+        
+
+	    rb.velocity = new Vector3(currentHorizontalSpeed, rb.velocity.y, rb.velocity.z);
+        //rb.rotation = new Quaternion(rb.rotation.x, rb.rotation.y, currentRotation, rb.rotation.w);
+        //rb.rotation.Set(rb.rotation.x, rb.rotation.y, 45, rb.rotation.w);
+	    rb.transform.eulerAngles = currentAngle;
+
+
+    }
+
+}
