5809cfab6dd94af4ce052967c5b4641f774f97c7
Albert Garcia Mestre <albert.garcia.2@enti.cat>
2019-06-19 01:55:42 +0200

    Enemy spawner and configuration of the sky of the map
    Spawning enemies that want to kill us, and configuration of the sky of the map

---
+++ Small-Plane-The-Travelling/Assets/Scripts/SkyBoxConfig.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyBoxConfig : MonoBehaviour {
+
+	public float skyBoxRotSpeed = 1f;
+    private bool rotationEnabled = true;
+	// Use this for initialization
+	void Start () {
+       
+    }
+
+    private void OnEnable()
+    {
+        GameStateManager.pauseGame += RotationDisabled;
+        GameStateManager.resumeGame += RotationEnabled;
+    }
+
+    private void OnDisable()
+    {
+        GameStateManager.pauseGame -= RotationDisabled;
+        GameStateManager.resumeGame -= RotationEnabled;
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+	{
+        if(rotationEnabled)
+        {
+            RenderSettings.skybox.SetFloat("_Rotation", Time.time * skyBoxRotSpeed);
+        }
+    }
+
+
+    /// Disabled Skybox rotation
+
+    private void RotationDisabled()
+    {
+        rotationEnabled = false;
+    }
+
+
+    /// Enables SkyBox rotation
+
+    private void RotationEnabled()
+    {
+        rotationEnabled = true;
+    }
+}
